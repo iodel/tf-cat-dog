11a34346f6bef9798f06f6c12f4d30137612703c
iodel <iodeliakos@gmail.com>
2019-05-29 03:49:53 +0300

    Init commit, create express app w/ ts

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+const add = (a: number, b: number): number => a + b;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log(add(5, 5));
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello from TensorFlow!</h2>
+</body>
+</html>`);
+});
+
+app.listen(5000, () => console.log('Server running...'));
